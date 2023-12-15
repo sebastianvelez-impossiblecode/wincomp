36bcad831862f5a962f9e4a2fbcc0a6e5930b768
svelezva <sebastian.velez@impossiblecode.ai>
2023-12-15 17:35:09 -0500

    [main] Test streamlit

---
+++ be/controllers/data_processing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def filter_dataframe():
+    pd.read_csv("")
+    pass
---
+++ fe/streamlit_app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.read_csv("full_harvest_data.csv", parse_dates=["siembra"])
+
+st.title("Demand Forecasting")
+
+st.sidebar.header("User input features")
+
+task = st.sidebar.radio(
+    "Select a task to perform",
+    ["df exploration", "Demand type visualization", "Product df visualization"],
+)
+
+if task == "df exploration":
+    category = st.sidebar.selectbox(
+        "Select a product category", pd.unique(df["variedad"])
+    )
+
+    date_range = st.sidebar.select_slider(
+        "Select a range of dates",
+        pd.unique(df["siembra"]),
+        value=(list(df["siembra"])[0], list(df["siembra"])[-1]),
+    )
+
+    display_df = df[
+        (df["variedad"] == category)
+        & (df["date"] >= date_range[0])
+        & (df["date"] <= date_range[1])
+    ]
+
+    st.markdown(
+        """
+        df dataset exploration:
+        """
+    )
+    st.dataframe(display_df, height=600)
